57911d3b54136b3ce8bc6aa359ac1158a27fe2c7
Javier Muñoz <x_jemd_x@hotmail.com>
2020-12-18 11:43:48 -0300

    cambios en vista wallet para ver datos de las diferentes cuentas

--- src/app/pages/sidemenu/wallet/wallet.page.ts
+++ src/app/pages/sidemenu/wallet/wallet.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/providers/auth/auth.service';
 
 @Component({
   selector: 'app-wallet',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletPage implements OnInit {
 
-  constructor() { }
+  user: User
+  userSelected: User
+
+  constructor(
+    private auth: AuthService,
+    private actionSheetController: ActionSheetController
+  ) { }
 
   ngOnInit() {
+    this.user = this.auth.userData()
+    this.userSelected = this.user
+  }
+
+  async changeUserSelected() {
+    let buttons = []
+    buttons.push({
+      text: this.user.firstname + ' ' + this.user.lastname,
+      handler: () => {
+        this.userSelected = this.user
+      }
+    })
+    this.user.elders.forEach(elder => {
+      buttons.push({
+        text: this.user.firstname + ' ' + elder.lastname,
+        handler: () => {
+          this.userSelected = elder
+        }
+      })
+    })
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Seleccionar usuario',
+      buttons
+    });
+    await actionSheet.present();
   }
 
 }
