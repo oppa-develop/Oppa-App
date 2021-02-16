c32c6089e6f222a10df71ac56b580f22f8bdc0f0
javier muñoz <x_jemd_x@hotmail.com>
2021-02-16 13:06:34 -0300

    menos tiempo en la carga falsa

--- src/app/providers/api/api.service.ts
+++ src/app/providers/api/api.service.ts
@@ -13,7 +13,7 @@ import * as timeago from 'timeago.js';
 })
 export class ApiService {
 
-  delay: number = 2000
+  delay: number = 1000
 
   constructor() { }
 
@@ -82,18 +82,18 @@ export class ApiService {
 
   getServices(): Observable<Service[]> {
     return of([
-      { id: parseInt(faker.random.uuid()), type: 'Servicio a Domicilio', name: 'peluquería', description: faker.lorem.paragraph(), price: parseInt('9990'), img: '../../../../assets/images/pexels-nick-demou-1319460.jpg' },
-      { id: parseInt(faker.random.uuid()), type: 'Servicio de acompañamiento', name: 'realizar trámite', description: faker.lorem.paragraph(), price: parseInt('9990'), img: '../../../../assets/images/1789259.jpg' },
-      { id: parseInt(faker.random.uuid()), type: 'Servicio a Domicilio', name: 'podología', description: faker.lorem.paragraph(), price: parseInt('14990'), img: '../../../../assets/images/pexels-stephanie-allen-4085445.jpg' },
-      { id: parseInt(faker.random.uuid()), type: 'Servicio de acompañamiento', name: 'cobro', description: faker.lorem.paragraph(), price: parseInt('14990'), img: '../../../../assets/images/pexels-eduardo-soares-5497951.jpg' },
-      { id: parseInt(faker.random.uuid()), type: 'Servicio a Domicilio', name: 'inyecciones', description: faker.lorem.paragraph(), price: parseInt('11990'), img: '../../../../assets/images/pexels-gustavo-fring-3985166.jpg' },
-      { id: parseInt(faker.random.uuid()), type: 'Servicio de acompañamiento', name: 'paseo', description: faker.lorem.paragraph(), price: parseInt('11990'), img: '../../../../assets/images/pexels-kaboompics-com-6054.jpg' },
-      { id: parseInt(faker.random.uuid()), type: 'Servicio a Domicilio', name: 'kinesiólogo', description: faker.lorem.paragraph(), price: parseInt('9990'), img: '../../../../assets/images/g-terapi-ile-agrilara-son-H1347048-11.jpg' },
-      { id: parseInt(faker.random.uuid()), type: 'Servicio de acompañamiento', name: 'salud', description: faker.lorem.paragraph(), price: parseInt('9990'), img: '../../../../assets/images/pexels-pixabay-40568.jpg' },
-      { id: parseInt(faker.random.uuid()), type: 'Servicio a Domicilio', name: 'cuidado', description: faker.lorem.paragraph(), price: parseInt('11990'), img: '../../../../assets/images/pexels-andrea-piacquadio-3768131.jpg' },
-      { id: parseInt(faker.random.uuid()), type: 'Servicio de acompañamiento', name: 'pagos', description: faker.lorem.paragraph(), price: parseInt('11990'), img: '../../../../assets/images/resize_1590967555.jpg' },
-      { id: parseInt(faker.random.uuid()), type: 'Servicio a Domicilio', name: 'curaciones', description: faker.lorem.paragraph(), price: parseInt('14990'), img: '../../../../assets/images/pexels-cottonbro-5721555.jpg' },
-      { id: parseInt(faker.random.uuid()), type: 'Servicio de acompañamiento', name: 'compras', description: faker.lorem.paragraph(), price: parseInt('14990'), img: '../../../../assets/images/pexels-gustavo-fring-4173326.jpg' }
+      { id: parseInt(faker.random.uuid()), type: 'Servicio a Domicilio',        name: 'peluquería',         description: faker.lorem.paragraph(), price: parseInt('9990'),  img: '../../../../assets/images/pexels-nick-demou-1319460.jpg' },
+      { id: parseInt(faker.random.uuid()), type: 'Servicio de acompañamiento',  name: 'realizar trámite',   description: faker.lorem.paragraph(), price: parseInt('9990'),  img: '../../../../assets/images/1789259.jpg' },
+      { id: parseInt(faker.random.uuid()), type: 'Servicio a Domicilio',        name: 'podología',          description: faker.lorem.paragraph(), price: parseInt('14990'), img: '../../../../assets/images/pexels-stephanie-allen-4085445.jpg' },
+      { id: parseInt(faker.random.uuid()), type: 'Servicio de acompañamiento',  name: 'cobro',              description: faker.lorem.paragraph(), price: parseInt('14990'), img: '../../../../assets/images/pexels-eduardo-soares-5497951.jpg' },
+      { id: parseInt(faker.random.uuid()), type: 'Servicio a Domicilio',        name: 'inyecciones',        description: faker.lorem.paragraph(), price: parseInt('11990'), img: '../../../../assets/images/pexels-gustavo-fring-3985166.jpg' },
+      { id: parseInt(faker.random.uuid()), type: 'Servicio de acompañamiento',  name: 'paseo',              description: faker.lorem.paragraph(), price: parseInt('11990'), img: '../../../../assets/images/pexels-kaboompics-com-6054.jpg' },
+      { id: parseInt(faker.random.uuid()), type: 'Servicio a Domicilio',        name: 'kinesiólogo',        description: faker.lorem.paragraph(), price: parseInt('9990'),  img: '../../../../assets/images/g-terapi-ile-agrilara-son-H1347048-11.jpg' },
+      { id: parseInt(faker.random.uuid()), type: 'Servicio de acompañamiento',  name: 'salud',              description: faker.lorem.paragraph(), price: parseInt('9990'),  img: '../../../../assets/images/pexels-pixabay-40568.jpg' },
+      { id: parseInt(faker.random.uuid()), type: 'Servicio a Domicilio',        name: 'cuidado',            description: faker.lorem.paragraph(), price: parseInt('11990'), img: '../../../../assets/images/pexels-andrea-piacquadio-3768131.jpg' },
+      { id: parseInt(faker.random.uuid()), type: 'Servicio de acompañamiento',  name: 'pagos',              description: faker.lorem.paragraph(), price: parseInt('11990'), img: '../../../../assets/images/resize_1590967555.jpg' },
+      { id: parseInt(faker.random.uuid()), type: 'Servicio a Domicilio',        name: 'curaciones',         description: faker.lorem.paragraph(), price: parseInt('14990'), img: '../../../../assets/images/pexels-cottonbro-5721555.jpg' },
+      { id: parseInt(faker.random.uuid()), type: 'Servicio de acompañamiento',  name: 'compras',            description: faker.lorem.paragraph(), price: parseInt('14990'), img: '../../../../assets/images/pexels-gustavo-fring-4173326.jpg' }
     ]).pipe(delay(this.delay));
   }
 
