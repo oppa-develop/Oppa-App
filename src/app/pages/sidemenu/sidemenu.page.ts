6ab595ebb5da529b2f60b65657c687e767dfda10
javier muñoz <x_jemd_x@hotmail.com>
2021-02-25 18:47:05 -0300

    cambios en nombre de vista mis datos y mi historial + user img genérica

--- src/app/pages/sidemenu/sidemenu.page.ts
+++ src/app/pages/sidemenu/sidemenu.page.ts
@@ -12,10 +12,10 @@ export class SidemenuPage implements OnInit {
 
   pages = [
     { title: 'Servicios',               icon: 'construct-outline',        url: '/sidemenu/services'},
-    { title: 'Cuenta',                  icon: 'person-outline',           url: '/sidemenu/account'},
+    { title: 'Mis Datos',               icon: 'person-outline',           url: '/sidemenu/account'},
     { title: 'Mensajes',                icon: 'chatbox-ellipses-outline', url: '/sidemenu/messages'},
     { title: 'Monedero',                icon: 'wallet-outline',           url: '/sidemenu/wallet'},
-    { title: 'Historial de Servicios',  icon: 'document-text-outline',    url: '/sidemenu/history'},
+    { title: 'Mis de Servicios',        icon: 'document-text-outline',    url: '/sidemenu/history'},
     { title: 'Ayuda',                   icon: 'help-circle-outline',      url: '/sidemenu/help'},
   ]
 
