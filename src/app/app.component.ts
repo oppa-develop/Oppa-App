e03888a5e969cfcf05f6e2ce630e3b10608c64f8
javier muñoz <x_jemd_x@hotmail.com>
2021-11-06 21:14:25 -0300

    nueva alerta para salir de la app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { AlertController, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 import { Autostart } from '@ionic-native/autostart/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -18,18 +19,51 @@ export class AppComponent {
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
     private screenOrientation: ScreenOrientation,
+    public router: Router,
+    private alertController: AlertController,
     private autostart: Autostart
   ) {
     this.setPortrait();
     this.initializeApp();
     this.autostart.enable();
   }
-  
+
   // set orientation to portrait
-  setPortrait(){
+  setPortrait() {
     this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
+
+    this.platform.backButton.subscribeWithPriority(10, () => {
+      if (this.router.url !== '/sidemenu/service') {
+        this.router.navigate(['/sidemenu/service']);
+      } else if (this.router.url === '/sidemenu/service') {
+        this.presentAlert();
+      }
+    });
   }
-  
+
+  async presentAlert() {
+    localStorage.setItem('createElderAccountAlert', 'done')
+    const alert = await this.alertController.create({
+      backdropDismiss: false,
+      header: 'Desea salir de la aplicación?',
+      buttons: [{
+        text: 'Cancelar',
+        role: 'cancel',
+        handler: () => {
+          console.log('cancela apadrinar');
+        }
+      }, {
+        text: 'Salir',
+        handler: async () => {
+          console.log('cerrando app');
+          navigator['app'].exitApp();
+        }
+      }]
+    })
+
+    await alert.present();
+  }
+
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
